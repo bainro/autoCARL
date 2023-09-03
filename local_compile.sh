cafbfde7bc4205b2252e0764dfac54bbf9a6db14
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-03 12:11:38 -0700

    Update local_compile.sh

--- local_compile.sh
+++ local_compile.sh
@@ -3,9 +3,9 @@ mkdir build
 cd ./build && rm -fr ./*
 cmake -DCMAKE_INSTALL_PREFIX=/tmp/autoCARL \
       -DCMAKE_BUILD_TYPE=Release .. \
-      -DCARLSIM_NO_CUDA=OFF \
+      -DCARLSIM_NO_CUDA=ON \
       -DCARLSIM_TEST=YES \
-      -DCARLSIM_PYCARL=ON \
-      -DCARLSIM_SHARED=ON \
-      -DCARLSIM_STATIC=OFF || cd .. # \--trace \--debug-output
+      -DCARLSIM_PYCARL=OFF \
+      -DCARLSIM_SHARED=OFF \
+      -DCARLSIM_STATIC=ON || cd .. # \--trace \--debug-output
 make -j8 install || cd ..
