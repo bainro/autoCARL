071eb85f40117866109b25960732772c11dd4c97
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-03 12:16:49 -0700

    Update local_compile.sh

--- local_compile.sh
+++ local_compile.sh
@@ -4,7 +4,7 @@ cd ./build && rm -fr ./*
 cmake -DCMAKE_INSTALL_PREFIX=/tmp/autoCARL \
       -DCMAKE_BUILD_TYPE=Release .. \
       -DCARLSIM_NO_CUDA=OFF \
-      -DCARLSIM_TEST=YES \
+      -DCARLSIM_TEST=OFF \
       -DCARLSIM_PYCARL=OFF \
       -DCARLSIM_SHARED=OFF \
       -DCARLSIM_STATIC=ON || cd .. # \--trace \--debug-output
