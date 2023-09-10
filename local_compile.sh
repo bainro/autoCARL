305e02ed0507b4b611c5716ee8eaad0c0657be29
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-09 20:03:24 -0700

    Update local_compile.sh

--- local_compile.sh
+++ local_compile.sh
@@ -10,7 +10,7 @@ cmake -DCMAKE_INSTALL_PREFIX=/tmp/autoCARL \
       -DCARLSIM_BENCHMARKS=OFF \
       -DCARLSIM_SHARED=OFF \
       -DCARLSIM_STATIC=ON \
-      --VERBOSE=1 \
+      -- VERBOSE=1 \
       --trace \
       --debug-output &> /tmp/bainro.log || cd ..
 make -j8 install &>> /tmp/bainro.log || cd ..
