5e9240cd38595cdbb692c9b7207ff21113c4b833
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-13 00:34:36 -0700

    Update local_compile.sh

--- local_compile.sh
+++ local_compile.sh
@@ -13,3 +13,10 @@ cmake -DCMAKE_INSTALL_PREFIX=/tmp/autoCARL \
       --trace \
       --debug-output || cd .. 
 make -j8 install VERBOSE=1 || cd ..
+
+### PRINT ALL CMAKE VAR'S
+#get_cmake_property(_variableNames VARIABLES)
+#list (SORT _variableNames)
+#foreach (_variableName ${_variableNames})
+#    message(STATUS "${_variableName}=${${_variableName}}")
+#endforeach()
