1933e3f40b89515e5cd7e287a5a57f52871864b5
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-23 02:28:51 -0700

    Update setup.py

--- pyCARL/setup.py
+++ pyCARL/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'carlsim',
   packages = ['carlsim'],
-  version = '1.1.2',
+  version = '1.1.3',
   license='MIT',
   description = 'GPU-accelerated Spiking Neural Network Simulator!',
   author = 'Robert Bain',
