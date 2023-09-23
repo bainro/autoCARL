8c1b5598beb977b1921ef80edac334d3b29d8cd4
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-23 13:11:53 -0700

    Update setup.py

--- pyCARL/setup.py
+++ pyCARL/setup.py
@@ -17,14 +17,6 @@ setup(
     'Intended Audience :: Education',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11'
+    'Programming Language :: Python :: 3'
   ],
 )
