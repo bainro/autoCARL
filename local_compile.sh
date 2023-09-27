d6a83eae512e3ba2ea8e0785d70031adba54a5b8
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-27 15:24:10 -0700

    Update local_compile.sh

--- local_compile.sh
+++ local_compile.sh
@@ -17,11 +17,13 @@ make -j8 install || cd ..
 
 # GCOV TESTING SUITE
 ./carlsim/test/carlsim-tests # assumes we're in build/
-# ./carlsim/test6/carlsim-tests6
-# lcov --directory ./ --capture --output-file ./code_coverage.lcov \
-#     -rc lcov_branch_coverage=1
-#lcov --remove ./code_coverage.lcov -o ./code_coverage.lcov \
-#     '/usr/*' '/tmp/*' '/home/rbain/github/autoCARL/build/*'
+./carlsim/test6/carlsim-tests6
+lcov --directory ./ --capture --output-file ./code_coverage.lcov \
+     -rc lcov_branch_coverage=1
+lcov --remove ./code_coverage.lcov -o ./code_coverage.lcov \
+     '/usr/*' '/tmp/*' '/home/rbain/github/autoCARL/build/*'
+
+cat ../code_coverage.lcov
 
 ### PRINT ALL CMAKE VAR'S. 
 # Useful for when CMAKE makes you want to die :) (ie debugging)
